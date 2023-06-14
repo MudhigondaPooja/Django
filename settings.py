2978cc01c0bbfb07736b51ab0a7887e262574e07
Pooja Mudhigonda <121855747+MudhigondaPooja@users.noreply.github.com>
2023-06-14 14:59:40 +0530

    Update settings.py

--- settings.py
+++ settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-y9d)$7tw^!xa$gajx4l39qt-ty7^pt#w$8m@e@1_qcy*-c3hvp
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['15.206.149.63','localhost', '0.0.0.0']
 
 
 # Application definition
